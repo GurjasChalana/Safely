d84d87c13ea0af1513bbb0cef6f09827e9bc8d61
Gurjas Chalana <gurjas619@gmail.com>
2026-03-28 14:09:31 -0400

    fix webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader',
+        use: { loader: 'ts-loader', options: { transpileOnly: true } },
         exclude: /node_modules/,
       },
     ],
